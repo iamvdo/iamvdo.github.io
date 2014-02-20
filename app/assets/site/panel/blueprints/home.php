b8a7e9280760e99dfd991d8b814bccb1a42a6065
iamvdo <vincent@iamvdo.me>
2014-02-20 14:54:57 +0100

    Add a home blueprint

---
+++ app/assets/site/panel/blueprints/home.php
@@ -0,0 +1,16 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+#Home
+#Used for categories
+
+title: Home
+pages: 
+  sort: flip
+files: false
+fields:
+  title: 
+    label: Title
+    type:  text
+  subtitle: 
+    label: Subtitle
+    type: text
