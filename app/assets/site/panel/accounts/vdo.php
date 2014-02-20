a9236ab98cbdb02f1cc6d6067e26dad5d4cd0056
Vincent De Oliveira <vincent@iamvdo.me>
2014-02-20 16:49:35 +0100

    Update account

---
+++ app/assets/site/panel/accounts/vdo.php
@@ -0,0 +1,11 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# setup your account credentials here
+# it's highly recommended to use md5 or sha1 encryption 
+# for your passwords. read more about encryption in the
+# docs: http://getkirby.com/docs/panel/accounts
+
+username: vdo
+password: c8d1609259c22eb7239c98965d72fb90
+encrypt: md5
+language: en
