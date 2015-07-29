11ec7294aed1814f3371abc6ecb1edac324e0951
Vincent De Oliveira <vincent@iamvdo.me>
2015-07-29 14:24:51 +0200

    Fix URLs in menu

--- app/public/site/snippets/menu.php
+++ app/public/site/snippets/menu.php
@@ -5,7 +5,7 @@
 		foreach($pages->find('blog', 'conf', 'lab', 'rss') AS $p) { 
 			?>
 			<li class="menu-item">
-				<a<?php echo ($p->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $p->url() ?>"><?php echo html($p->title()) ?></a>
+				<a<?php echo ($p->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $p->uri() ?>"><?php echo html($p->title()) ?></a>
 			</li>
 		<?php 
 		}
