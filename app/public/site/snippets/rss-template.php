cf3261cd93308498d0b7a41e7688acd24e290f23
Vincent De Oliveira <vincent@iamvdo.me>
2015-08-10 17:12:33 +0200

    Fix RSS (fix #2)

--- app/public/site/snippets/rss-template.php
+++ app/public/site/snippets/rss-template.php
@@ -9,15 +9,16 @@ header('Content-type: text/xml; charset="utf-8"');
 // echo the doctype
 echo '<?xml version="1.0" encoding="utf-8"?>';
 
+date_default_timezone_set('Europe/Paris');
 ?>
 
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
 
   <channel>
     <title><?php echo (isset($title)) ? xml($title) : xml($site->title()) ?></title>
-    <link><?php echo $site->full_url(); echo (isset($link)) ? xml($link) : xml(url()) ?></link>
-    <lastBuildDate><?php echo (isset($modified)) ? date('r', $modified) : date('r', $site->modified()) ?></lastBuildDate>
-    <atom:link href="<?php echo $site->full_url(); echo xml(thisURL()) ?>" rel="self" type="application/rss+xml" />
+    <link><?php echo $site->full_url() . $site->homePage()->url(); ?></link>
+    <lastBuildDate><?php echo (isset($modified)) ? date('D, d M Y H:i:s T', $modified) : date('D, d M Y H:i:s T', $site->modified()) ?></lastBuildDate>
+    <atom:link href="<?php echo xml(thisURL()) ?>" rel="self" type="application/rss+xml" />
 
     <?php if($page->description() || isset($description)): ?>
     <description><?php echo (isset($description)) ? xml($description) : xml($page->description()) ?></description>
@@ -28,7 +29,7 @@ echo '<?xml version="1.0" encoding="utf-8"?>';
       <title><?php echo xml($item->title()) ?></title>  
       <link><?php echo $site->full_url(); echo xml($item->url()) ?></link>
       <guid><?php echo $site->full_url(); echo xml($item->url()) ?></guid>
-      <pubDate><?php echo ($item->date()) ? date('r', $item->date()) : date('r', $item->modified()) ?></pubDate>
+      <pubDate><?php echo ($item->date()) ? date('D, d M Y H:i:s T', $item->date()) : date('D, d M Y H:i:s T', $item->modified()) ?></pubDate>
         
       <?php if(isset($descriptionField)): ?>
       <?php if(!$descriptionExcerpt): ?>
