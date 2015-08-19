1ca6557ced4da98b201ad52a3c510b9a95289c16
Vincent De Oliveira <vincent@iamvdo.me>
2015-08-19 11:51:21 +0200

    Add Codepen placeholder

--- app/public/site/tags/codepen.php
+++ app/public/site/tags/codepen.php
@@ -12,7 +12,11 @@ kirbytext::$tags['codepen'] = array(
     $height = $tag->attr('height');
     $script = $tag->attr('script');
 
-    $html = '<p data-height="' . $height . '" data-theme-id="0" data-slug-hash="' . $hash . '" data-user="' . $user . '" data-default-tab="result" class="codepen">See the Pen <a href="http://codepen.io/' . $user . '/pen/' . $hash . '">' . $hash . '</a> by ' . $user . ' (<a href="http://codepen.io/' . $user . '">@' . $user . '</a>) on <a href="http://codepen.io">CodePen</a></p>';
+    $html = '<div class="codepen-placeholder" style="height:' . $height . 'px">';
+
+    $html .= '<p data-height="' . $height . '" data-theme-id="0" data-slug-hash="' . $hash . '" data-user="' . $user . '" data-default-tab="result" class="codepen">See the Pen <a href="http://codepen.io/' . $user . '/pen/' . $hash . '">' . $hash . '</a> by ' . $user . ' (<a href="http://codepen.io/' . $user . '">@' . $user . '</a>) on <a href="http://codepen.io">CodePen</a></p>';
+
+    $html .= '</div>';
     // Inserting a script tag creates a bug with markdown
     // $html .= (isset($script)) ? '<script async src="//assets.codepen.io/assets/embed/ei.js"></script>' : '';
 
