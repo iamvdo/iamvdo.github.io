08978e763556f977f411a7f77a679028da489de7
Vincent De Oliveira <vincent@iamvdo.me>
2016-02-12 14:59:31 +0100

    Fix URL for english RSS

--- app/public/site/snippets/menubar.php
+++ app/public/site/snippets/menubar.php
@@ -5,7 +5,7 @@
       <li class="menu-item"><a href="http://twitter.com/iamvdo">Twitter</a></li>
       <li class="menu-item"><a href="http://dribbble.com/iamvdo">Dribbble</a></li>
       <li class="menu-item"><a href="http://github.com/iamvdo">GitHub</a></li>
-      <li class="menu-item"><a href="http://iamvdo.me/rss">RSS</a></li>
+      <li class="menu-item"><a href="<?php echo $site->full_url() . $site->homePage()->url(); ?>/rss">RSS</a></li>
       <li class="menu-item">vincent@iamvdo.me</li>
     </ul>
   </nav>
