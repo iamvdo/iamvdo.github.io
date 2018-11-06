479fa1a6700d8078cc1e568dbd67ced66393be2d
Vincent De Oliveira <vincent@iamvdo.me>
2018-11-06 15:50:35 +0100

    Add Matomo analytics

--- app/public/site/snippets/script.php
+++ app/public/site/snippets/script.php
@@ -13,3 +13,15 @@
     ga('send', 'event', 'Flattr', 'Click');
   });
 </script>
+<script>
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.iamvdo.me/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
