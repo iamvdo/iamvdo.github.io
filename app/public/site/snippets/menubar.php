c50f5286f2812c46da49ed5ade8248aab4ff5f62
Vincent De Oliveira <vincent@iamvdo.me>
2020-01-15 15:17:30 +0100

    Do not show email as is

--- app/public/site/snippets/menubar.php
+++ app/public/site/snippets/menubar.php
@@ -6,7 +6,12 @@
       <li class="menu-item"><a href="http://dribbble.com/iamvdo">Dribbble</a></li>
       <li class="menu-item"><a href="http://github.com/iamvdo">GitHub</a></li>
       <li class="menu-item"><a href="<?php echo $site->full_url() . $site->homePage()->url(); ?>/rss">RSS</a></li>
-      <li class="menu-item">vincent@iamvdo.me</li>
+      <li class="menu-item" id="myemail">...</li>
+      <script>
+        let a = 'vincent';
+        let b = 'iamvdo';
+        myemail.innerHTML = a + '@' + b + '.me';
+      </script>
     </ul>
   </nav>
 </div>
